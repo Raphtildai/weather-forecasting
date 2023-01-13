56eb4f8ee6a82b4c0f65cd6773b4956e1569a4b0
Tildai <raphaeltildai5@gmail.com>
2023-01-13 10:26:09 +0300

    Initial commit of the files

---
+++ js/index.js
@@ -0,0 +1,64 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '59232e6ddcmsh1d0ad70d522ed9cp15163fjsn2176521e863f',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+let weather = {
+    fetchWeather: function (city) {
+        fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city +'', options)
+        .then(response => response.json())
+        .then(response => this.displayweather(response, city))
+        .catch(err => console.error(err));
+    },
+	
+	// function to display weather
+	displayweather: function (data, city) {
+        const diff = data.max_temp - data.min_temp;
+		document.querySelector(".city").innerText = "Weather in " + city;
+		document.querySelector(".icon").src = "https://openweathermap.org/img/wn/03d.png"
+		document.querySelector(".temp").innerText = data.temp + " °c";
+		document.querySelector(".description").innerText = "Temp range: "  + diff;
+		document.querySelector(".humidity").innerText = "Humidity: " + data.humidity + " %";
+		document.querySelector(".wind").innerText = "Wind Speed: " + data.wind_speed + " km/hr";	
+		document.querySelector(".weather").classList.remove("loading");
+		document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ city +"')";
+	},
+	search: function () {
+		this.fetchWeather(document.querySelector(".search-bar").value);
+	}
+};
+
+// Fetching city from the user
+document.querySelector(".search button").addEventListener(
+	"click", 
+	function () {
+		weather.search();
+});
+
+// Adding event listener when the user clicks enter
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+	if(event.key == "Enter"){
+		weather.search()
+	}
+});
+
+//Data to be loaded when the page first loads
+weather.fetchWeather("Eldoret");
+
+// pallindrom
+// function isPalindrome(word) {
+//     const length = word.length;
+//     const half = Math.floor(length / 2);
+//     for (let index = 0; index < half; index++) {
+//       if (word[index] !== word[length - index - 1]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//   isPalindrome('madam'); // => true
+//   isPalindrome('hello'); // => false
+  
